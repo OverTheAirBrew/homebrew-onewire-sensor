b59dc33acac180f16565501f0bae7fc6756f5093
Nick Sharp <nick.sharp.87@gmail.com>
2021-10-07 14:06:37 +0100

    feat: including locales

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { OneWireSensor } from './one-wire-sensor';
 import { IPackageConfig } from '@overtheairbrew/homebrew-plugin';
+import { join } from 'path';
+import { OneWireSensor } from './one-wire-sensor';
 
 const OtaPackage: IPackageConfig = {
   sensors: [OneWireSensor],
+  localesDirectory: join(__dirname, 'locales'),
 };
 
 export default OtaPackage;
